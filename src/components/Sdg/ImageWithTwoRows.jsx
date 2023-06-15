f9cc46cfed72859b57514f7dfd83e51711816022
alimrts <alimrts07@gmail.com>
2023-06-15 18:08:07 +0300

    sdg edit

--- src/components/Sdg/ImageWithTwoRows.jsx
+++ src/components/Sdg/ImageWithTwoRows.jsx
@@ -1,23 +1,106 @@
-import React from "react";
-import "./Sdg.css";
+// import React from "react";
+// import "./Sdg.css";
 
-const ImageWithTwoRows = ({ src, row1 }) => {
+// const ImageWithTwoRows = ({ src, row1 }) => {
+//   return (
+//     <div className="imageWithRow">
+//       <img src={src} alt="" style={{ margin: "6px", height: "150px" }} />
+//       <div style={{ display: "flex", flexDirection: "column" }}>
+//         <span
+//           style={{
+//             marginBottom: "5px",
+//             marginTop: "5px",
+//             fontSize: "14pt",
+//             lineHeight: "1.2",
+//           }}
+//         >
+//           {row1}
+//         </span>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default ImageWithTwoRows;
+import React, { useState } from "react";
+
+const ImageWithTwoRows = ({ src1, src2, row1, rowTitle }) => {
+  const [hovered, setHovered] = useState(false);
+
+  const [showTextPopup, setShowTextPopup] = useState(false);
+
+  const handleImageHover = () => {
+    setHovered(true);
+  };
+
+  const handleImageHoverLeave = () => {
+    setHovered(false);
+  };
+
+  const handleImageClick = () => {
+    setShowTextPopup(!showTextPopup);
+  };
+
+  const handleTextClose = () => {
+    setShowTextPopup(!showTextPopup);
+  };
+
   return (
-    <div className="imageWithRow">
-      <img src={src} alt="" style={{ margin: "6px", height: "150px" }} />
-      <div style={{ display: "flex", flexDirection: "column" }}>
-        <span
-          style={{
-            marginBottom: "5px",
-            marginTop: "5px",
-            fontSize: "14pt",
-            lineHeight: "1.2",
-          }}
+    <>
+      <div className="imageWithRowEdit">
+        <div
+          className="imageWrapperEdit"
+          onMouseEnter={handleImageHover}
+          onMouseLeave={handleImageHoverLeave}
         >
-          {row1}
-        </span>
+          <div className={"imageContainer"}>
+            <img
+              src={src2}
+              alt=""
+              style={{
+                margin: "6px",
+                span: "6px",
+                height: "400px",
+                position: "absolute",
+                filter: !hovered ? "blur(4px)" : "none",
+                zIndex: !hovered ? -1 : 1,
+                borderRadius: "5px",
+                boxShadow: "0 2px 4px rgba(0, 0, 0, 0.2)",
+              }}
+            />
+
+            <img
+              src={src1}
+              alt=""
+              style={{
+                margin: "6px",
+                span: "6px",
+                height: "150px",
+                borderRadius: "5px",
+                boxShadow: "0 12px 18px rgba(0, 0, 0, 0.6)",
+              }}
+            />
+          </div>
+        </div>
       </div>
-    </div>
+      {hovered && (
+        <div
+          className="textPopup"
+          style={{ pointerEvents: !hovered ? "auto" : "none" }}
+        >
+          <span
+            style={{
+              color: "orange",
+            }}
+          >
+            {rowTitle}
+            <br /> <br />
+          </span>
+          <span>{row1}</span>
+          {/* <button onClick={handleTextClose}> Kapat</button> */}
+        </div>
+      )}
+    </>
   );
 };
 
--- src/components/Sdg/WrapperComponent.jsx
+++ src/components/Sdg/WrapperComponent.jsx
@@ -2,32 +2,245 @@ import React from "react";
 
 import ImageWithTwoRows from "./ImageWithTwoRows";
 import sdg1 from "../../img/sdg1.jpg";
+import sdg1_1 from "../../img/ss1.jpg";
 import sdg2 from "../../img/sdg2.jpg";
+import sdg2_2 from "../../img/ss2.jpg";
 import sdg3 from "../../img/sdg3.jpg";
+import sdg3_3 from "../../img/ss3.jpg";
 import sdg4 from "../../img/sdg4.jpg";
+import sdg4_4 from "../../img/ss4.jpg";
 import sdg5 from "../../img/sdg5.jpg";
+import sdg5_5 from "../../img/ss5.jpg";
 import sdg6 from "../../img/sdg6.jpg";
+import sdg6_6 from "../../img/ss6.jpg";
 import sdg7 from "../../img/sdg7.jpg";
+import sdg7_7 from "../../img/ss7.jpg";
 import sdg8 from "../../img/sdg8.jpg";
+import sdg8_8 from "../../img/ss8.jpg";
 import sdg9 from "../../img/sdg9.jpg";
+import sdg9_9 from "../../img/ss9.jpg";
 import sdg10 from "../../img/sdg10.jpg";
+import sdg10_10 from "../../img/ss10.jpg";
 import sdg11 from "../../img/sdg11.jpg";
+import sdg11_11 from "../../img/ss11.jpg";
 import sdg12 from "../../img/sdg12.jpg";
+import sdg12_12 from "../../img/ss12.jpg";
 import sdg13 from "../../img/sdg13.jpg";
+import sdg13_13 from "../../img/ss13.jpg";
 import sdg14 from "../../img/sdg14.jpg";
+import sdg14_14 from "../../img/ss14.jpg";
 import sdg15 from "../../img/sdg15.jpg";
+import sdg15_15 from "../../img/ss15.jpg";
 import sdg16 from "../../img/sdg16.jpg";
+import sdg16_16 from "../../img/ss16.jpg";
 import sdg17 from "../../img/sdg17.jpg";
+import sdg17_17 from "../../img/ss17.jpg";
 
 const WrapperComponent = () => {
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-      }}
-    >
-      <ImageWithTwoRows
+    <>
+      <div className="sdgGrid-container">
+        <ImageWithTwoRows
+          src1={sdg1}
+          src2={sdg1_1}
+          rowTitle="Yoksulluğa Son"
+          row1="Dünyanın her yerinde yoksulluğu sona erdirmek birinci amaçtır. Bu amaç, herkesin yiyecek,
+barınma, giyim, sağlık ve eğitime ulaşmasını böylece topluma tam anlamıyla katılabilmesini
+sağlayacaktır. Yoksul olmak demek çoğu kez sosyal güvenlik, yeni teknoloji, mal-mülk, arazi sahipliği
+ve mali yardım gibi temel şeylere erişememek anlamına geliyor. Bu amaç bunların hepsini
+düzeltmeyi hedefliyor. İklimin ve diğer doğal afetlerin, sosyal ve ekonomik sarsıntıların etkilediği
+insanlara yardım etmemiz gerekir."
+        />{" "}
+        <ImageWithTwoRows
+          src1={sdg2}
+          src2={sdg2_2}
+          rowTitle="Açlığa Son"
+          row1="Herkese yetecek kadar yiyecek varken, çocuklar dahil birçok insan hala açlık çekiyor. Amaç 2, hiç
+kimsenin açlık çekmemesini sağlamak için, yetersiz tarım uygulamaları, yiyecek israfı ve çevrenin
+bozulmasını ele alıyor. Bugün ve gelecekte, doğaya zarar vermeden gıda üretimini teşvik ediyor."
+        />
+        <ImageWithTwoRows
+          src1={sdg3}
+          src2={sdg3_3}
+          rowTitle="Sağlık ve Kaliteli Yaşam"
+          row1="İnsanların sağlığı yerinde olunca, toplumların refahı artar. Son yıllarda HIV/AIDS, sıtma ve diğer
+hastalıkların etkilerini azaltmak için çok çaba gösterilmekle birlikte, gerçek ilerleme ancak ve
+ancak kadınlar ve çocuklar dahil herkes yeterli sağlık hizmetine erişebildiğinde gerçekleşmiş olur.
+Amaç 3, tüm dünyada anne, bebek ve çocuk ölümlerini azaltma, aile planlamasını destekleme,
+madde bağımlılığını önleme ve tedavi etme, trafik kazalarının sayısını yarı yarıya azaltma gibi
+temel hedefleri içeriyor. Dünyada salgınları sona erdirme, herkes için erişilebilir aşı ve zorunlu
+ilaçlara erişim sağlama, kimyasal ve biyolojik kirlilik ve insanlara bulaşma riskini ortadan
+kaldırmaya acilen ihtiyaç var. Dünyanın en kırılgan bölgelerinde doktor ve hemşire sayısını
+artırmadan bu amaca ulaşamayız."
+        />
+        <ImageWithTwoRows
+          src1={sdg4}
+          src2={sdg4_4}
+          rowTitle="Nitelikli Eğitim"
+          row1="Eğitime erişim insanların yoksulluktan kurtulmasını, çevremizdeki dünyayı daha iyi anlamasını,
+kızlar dahil herkes için daha iyi olanaklar sunulmasını sağlar. Amaç 4, kim ve nerede olursa olsun
+tüm insanların eğitime erişebilmesini sağlamayı hedefliyor. Zor durumdaki çocuklar, engelliler ve
+yerli halklar dahil kırılgan insanların nitelikli eğitim alabilmeleri gerekiyor. Amaç 4, tüm insanların
+insan haklarını, barışı ve şiddetsizliği destekleyecek biçimde yetiştirilmesinin önemini ve
+böylelikle uyum içinde bir arada yaşayabileceğimizi vurguluyor."
+        />
+        <ImageWithTwoRows
+          src1={sdg5}
+          src2={sdg5_5}
+          rowTitle="Toplumsal Cinsiyet Eşitliği"
+          row1="Kadınlar ve kız çocukları hala ayrımcılık ve şiddete uğruyor; unutmayın ki dünya nüfusunun yarısını
+        onlar oluşturuyor! Amaç 5, eğitim, sağlık ve insana yakışır işler de dahil olmak üzere her bakımdan
+        toplumsal cinsiyet eşitliğini hedefliyor, ki bunların tümü topluma yarar sağlar. Dünyanın her
+        yerinde kadınlar ve kız çocuklarına yönelik, zorla evlendirme ve kadın sünneti dahil her türlü
+        ayrımcılık ve şiddetin ortadan kaldırılması bu amaca ulaşmanın temelidir. Ayrıca, kadınlar ve kız
+        çocuklarının cinsellik ve üreme ile ilgili insan haklarına tam erişime sahip olmalarını sağlamamız
+        gerekiyor. Kadınlar, mülke sahip olabilmeli, mali hizmetler ve yeni teknolojilere erişebilmeli,
+        toplumlarında siyasi, ekonomik ve kamusal yaşama tüm düzeylerde katılabilmelidir. Yönetici,
+        işletme yöneticisi, meclis üyesi kadın sayısının artırılması da bu amacın temel hedeflerindendir."
+        />
+        <ImageWithTwoRows
+          src1={sdg6}
+          src2={sdg6_6}
+          rowTitle="Temiz Su ve Sanitazyon"
+          row1="Dünyada milyarlarca insanın, çoğumuzun kanıksadığı bir insan hakkı olan, temiz su ve tuvaletlere
+        erişimi bulunmamaktadır. Su yaşamın temeli ve hiçbir şekilde kirletilmemeli, herkes tarafından
+        geri dönüştürülmeli ve yeniden kullanılmalıdır."
+        />
+        <ImageWithTwoRows
+          src1={sdg7}
+          src2={sdg7_7}
+          rowTitle="Erişilebilir ve Temiz Enerji"
+          row1="Pişirme ve ısınma için hala odun ve odun kömürü kullanan milyarlarca insana karşılanabilir
+        ve güvenilir enerji sağlamalıyız. Amaç 7 aynı zamanda, iklim değişikliği ile mücadeleye
+        katkıda bulunmak için temiz ve yenilenebilir enerjiye ihtiyaç duyduğumuzu vurguluyor.
+        Enerji modern, ucuz, güvenilir ve sürdürülebilir biçimde üretilmeli. Mümkün olduğunca
+        temiz olmalı ve güneş ve rüzgar gibi kirlenme yaratmayan kaynaklardan elde edilmeli. Bu
+        amaç, gelişmekte olan ülkelere modern enerji biçimleri arzını desteklemeyi ve artırmayı,
+        tüm ülkelerin bir araya gelerek, tüm insanlara ve Yerküremize yarar sağlayacak biçimde
+        temiz enerji araştırmalarını desteklemesi ve teknolojiyi geliştirmesini hedefliyor."
+        />
+        <ImageWithTwoRows
+          src1={sdg8}
+          src2={sdg8_8}
+          rowTitle="İnsana Yakışır İş ve Ekonomik Büyüme"
+          row1="Bir yanda küresel işsizlik artarken, daha çok iş yaratmanın yollarını bulmamız gerekiyor.
+        Amaç 8, ekonomiyi canlandıran ve hem kadınlar hem de erkeklere eşit fırsatlar sağlayan,
+        bir yanda da çevreyi koruyan daha çok işin yaratılmasını hedefliyor. Kadınlar, erkekler,
+        gençler, engelliler dahil herkesin iş sahibi olmasını ve eşit değerde iş için eşit ücret almasını
+        sağlamayı amaçlıyor."
+        />
+        <ImageWithTwoRows
+          src1={sdg9}
+          src2={sdg9_9}
+          rowTitle="Sanayi, Yenilikçilik ve Altyapı"
+          row1="Basit ifadeyle, Amaç 9, toplumların büyümesi için, bir yanda çevreyi korurken diğer yanda
+        herkese eşit fırsatlar sağlayan sektörlerin teşvik edilmesi gerektiğini söylüyor. Bu
+        sektörlerin aynı zamanda güvenilir ulaşım ve teknolojik yenilikçilik gibi dayanıklı altyapılar
+        ile de desteklenmesini amaçlıyor."
+        />
+        <ImageWithTwoRows
+          src1={sdg10}
+          src2={sdg10_10}
+          rowTitle="Eşitsizliklerin Azaltılması"
+          row1="Toplumda gerçek ilerleme sağlanması için, insanların birey olarak gelişmesini
+        sağlayacak fırsatlara herkesin erişebilmesi gerekiyor. Ancak birçok yerde insanlar
+        toplumsal cinsiyet, engellilik, etnik veya ırksal grup veya geçmişi nedeniyle
+        ayrımcılıkla karşı karşıya kalıyor. Amaç 10 herkesin sağlıklı ve mutlu bir yaşam
+        sürme olanağına sahip olmasını hedefliyor."
+        />
+        <ImageWithTwoRows
+          src1={sdg11}
+          src2={sdg11_11}
+          rowTitle="Sürdürülebilir Şehirler ve Topluluklar"
+          row1="Şehirlerimiz ve köylerimizin temiz ve güvenli olması, yeterli barınma ve elektriksu
+        gibi temel hizmetlere sahip olması gerekiyor. Aynı zamanda, herkesin keyifle
+        kullanabileceği temiz ve verimli ulaşım sistemleri ve yeşil alanlar olması gerek.
+        İnsanların afetler ve iklim değişikliği etkilerine karşı korunması gerek. Önemli
+        kültürel ve doğal alanların herkes için korunması gerek. Hava kalitesi ve atık
+        yönetimini iyileştirmemiz, bir yandan da gelişmekte olan ülkelerde yerli
+        malzeme ile daha iyi binalar yapılmasını desteklememiz gerekiyor."
+        />
+        <ImageWithTwoRows
+          src1={sdg12}
+          src2={sdg12_12}
+          rowTitle="Sorumlu Üretim ve Tüketim"
+          row1="Amaç 12, kullandığımız şeyler, yarattığımız atıklar ve bunların Yerküremizi nasıl
+        etkilediği hakkında iki kez düşünmemizi istemektedir. Geri dönüşüm gibi daha
+        sürdürülebilir eylemlere doğru davranışlarımızı değiştirmek, bireyler, işletmeler,
+        hükümetler dahil herkes katkıda bulununca gerçekten fark yaratır. Bu amaca
+        ulaşmak için hepimizin yapabileceği birçok şey var. Amaç 12, dünyamızda küçük
+        bir azınlık için değil herkes için yaşam kalitesini yükseltmeyi hedefliyor."
+        />
+        <ImageWithTwoRows
+          src1={sdg13}
+          src2={sdg13_13}
+          rowTitle="İklim Eylemi"
+          row1="İklim sürekli değişim halindedir, ancak son 200 yılda, insan faaliyetleri nedeniyle bu
+        değişiklikler daha fazla olmaktadır. İklim değişikliği artık tüm ülkeleri etkiliyor, en çok
+        da en yoksul ve kırılgan insanlar etkileniyor. Amaç 13, iklim değişikliği ile mücadele
+        için yenilenebilir enerji ve temiz teknolojiler gibi çözümler bulmayı hedefliyor.
+        Gelişmekte olan ülkeler ve küçük ada devletlerinin özellikle kadınlar, gençler, yerel
+        toplumlar ve dezavantajlı kesimlere odaklanarak çevre değişimi sonuçlarıyla başa
+        çıkabilmeleri için desteğe ihtiyaçları var."
+        />
+        <ImageWithTwoRows
+          src1={sdg14}
+          src2={sdg14_14}
+          rowTitle="Sudaki Yaşam"
+          row1="Amaç 14 okyanuslar, denizler ve içinde yaşayan türlerin tümünü korumayı hedefliyor.
+        Çünkü okyanuslar yiyecek, ilaç, biyo-yakıt ve milyonlarca insana iş sağlıyor.
+        Okyanuslara iyi bakarsak iklim değişikliğiyle baş etmemize katkısı olacak. Bizim
+        sağlıklı okyanuslara ihtiyacımız var, okyanusların da bizim korumamıza! Özellikle bitki
+        ve hayvan yaşamına büyük zarar veren ve türlerin neslini tehlikeye sokan plastik çöp
+        yığınlarını azaltmak için hep birlikte çalışmak zorundayız."
+        />
+        <ImageWithTwoRows
+          src1={sdg15}
+          src2={sdg15_15}
+          rowTitle="Karasal Yaşam"
+          row1="Hepimiz küresel ekosistemin bir parçasıyız. Amaç 15, kendi küresel evimizi tehdit
+        eden faaliyetlerimizi durdurmayı hedefliyor. Durdurulması gerekenler arasında
+        ormanların yok edilmesi, arazi bozunumu, hayvan ve bitki türlerinin kaybı var. Doğa,
+        insanların yaşamlarına o kadar büyük katkıda bulunuyor ki doğayı korumak
+        zorundayız. Bu amaç, korunan bitki ve hayvan türlerinin kaçak avlanması ve
+        kaçırılmasına son vermek için acil önlemler almayı, doğayı korumak için
+        harcamalarımızı artırmayı ve gelişmekte olan ülkelerin de bunu yapabilmesine yardım
+        etmeyi gerektiriyor."
+        />
+        <ImageWithTwoRows
+          src1={sdg16}
+          src2={sdg16_16}
+          rowTitle="Barış, Adalet ve Güçlü Kurumlar"
+          row1="Amaç 16, herkesin adalete erişebileceği, korku
+        duymadan hayatını sürdüreceği barışçıl toplumlarda
+        yaşamasını sağlayacak yollar bulmayı
+        hedeflemektedir. Dünyanın her yerinde çocuklar,
+        genç kadınlar ve erkeklere yönelik her türlü taciz,
+        istismar, kaçırma, şiddet ve işkenceye son vermeyi
+        hedefliyor. Tüm insanlar kendi ülkelerindeki kamusal
+        yaşama katılmaya teşvik ediliyor."
+        />
+        <ImageWithTwoRows
+          src1={sdg17}
+          src2={sdg17_17}
+          rowTitle="Amaçlar İçin Ortalıklar"
+          row1="Amaçlara ulaşmak için hükümetler, özel sektör, sivil
+        toplum ve bizim gibi insanlar dahil herkesin sürece
+        katılması gerekmektedir. Gelişmiş ülkeler, gelişmekte
+        olan ülkelere daha çok mali, bilimsel ve teknolojik
+        yardım sağlamalı, ürünlerini uluslararası pazarlara
+        daha çok satabilmelerine ve yerel kapasitelerini
+        güçlendirmelerine destek vermelidir."
+        />
+      </div>
+
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        {/* <ImageWithTwoRows
         src={sdg1}
         row1="Dünyanın her yerinde yoksulluğu sona erdirmek birinci amaçtır. Amaç 1, herkesin yiyecek,
 barınma, giyim, sağlık ve eğitime ulaşmasını böylece topluma tam anlamıyla katılabilmesini
@@ -35,8 +248,9 @@ sağlayacak. Yoksul olmak demek çoğu kez sosyal güvenlik, yeni teknoloji, mal
 ve mali yardım gibi temel şeylere erişememek anlamına geliyor. Bu amaç bunların hepsini
 düzeltmeyi hedefliyor. İklimin ve diğer doğal afetlerin, sosyal ve ekonomik sarsıntıların etkilediği
 insanlara yardım etmemiz gerekir."
-      />
-      <ImageWithTwoRows
+      /> */}
+
+        {/* <ImageWithTwoRows
         src={sdg2}
         row1="Herkese yetecek kadar yiyecek varken, çocuklar dahil birçok insan hala açlık çekiyor. Amaç 2, hiç
 kimsenin açlık çekmemesini sağlamak için, yetersiz tarım uygulamaları, yiyecek israfı ve çevrenin
@@ -184,8 +398,9 @@ böylelikle uyum içinde bir arada yaşayabileceğimizi vurguluyor."
         yardım sağlamalı, ürünlerini uluslararası pazarlara
         daha çok satabilmelerine ve yerel kapasitelerini
         güçlendirmelerine destek vermelidir."
-      />
-    </div>
+      /> */}
+      </div>
+    </>
   );
 };
 
