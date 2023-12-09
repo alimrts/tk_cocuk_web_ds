0a11b14d7cc0cfc900073d273a8b46117d9abefd
alimrts <alimrts07@gmail.com>
2023-12-09 23:03:35 +0300

    edit1

--- src/components/Intro/Model.js
+++ src/components/Intro/Model.js
@@ -11,7 +11,7 @@ export default function Model({ ...props }) {
       group.current.rotation.y += 0.005;
     }
   });
-  const { nodes, materials, animations } = useGLTF('/models/lab.glb')
+  const { nodes, materials, animations } = useGLTF('../models/lab.glb')
   const { actions } = useAnimations(animations, group)
 
   useEffect(() => {
@@ -181,4 +181,4 @@ export default function Model({ ...props }) {
   )
 }
 
-useGLTF.preload('/models/lab.glb')
+useGLTF.preload('../models/lab.glb')
