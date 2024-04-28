ab58c4a05f0a28602a20ea37787ad942c0e1c3a9
alimrts <alimrts07@gmail.com>
2024-04-28 23:52:03 +0300

    player touch update

--- src/components/metaworld/JoystickButtons.js
+++ src/components/metaworld/JoystickButtons.js
@@ -1,3 +1,118 @@
+// import React from "react";
+// import "./JoystickButtons.css";
+
+// import useZustandStore from "../../zustandStore";
+
+// import leftButtonImage from "./joystick_ui/left_arrow.png";
+// import rightButtonImage from "./joystick_ui/right_arrow.png";
+// import upButtonImage from "./joystick_ui/up_arrow.png";
+// import downButtonImage from "./joystick_ui/down_arrow.png";
+
+// const JoystickButtons = () => {
+//   const setIsLeftButtonPressed = useZustandStore(
+//     (state) => state.setIsLeftButtonPressed
+//   );
+
+//   const setIsRightButtonPressed = useZustandStore(
+//     (state) => state.setIsRightButtonPressed
+//   );
+
+//   const setIsUpButtonPressed = useZustandStore(
+//     (state) => state.setIsUpButtonPressed
+//   );
+
+//   const setIsDownButtonPressed = useZustandStore(
+//     (state) => state.setIsDownButtonPressed
+//   );
+
+//   const handleLeftButtonClick = () => {
+//     setIsLeftButtonPressed(true);
+//   };
+//   const handleLeftButtonRelease = () => {
+//     setIsLeftButtonPressed(false);
+//   };
+
+//   const handleRightButtonClick = () => {
+//     setIsRightButtonPressed(true);
+//   };
+//   const handleRightButtonRelease = () => {
+//     setIsRightButtonPressed(false);
+//   };
+
+//   const handleUpButtonClick = () => {
+//     setIsUpButtonPressed(true);
+//   };
+//   const handleUpButtonRelease = () => {
+//     setIsUpButtonPressed(false);
+//   };
+
+//   const handleDownButtonClick = () => {
+//     setIsDownButtonPressed(true);
+//   };
+//   const handleDownButtonRelease = () => {
+//     setIsDownButtonPressed(false);
+//   };
+
+//   /////
+
+//   return (
+//     <div className="joystick-buttons">
+//       <button
+//         className="leftButtonNav"
+//         // onMouseDown={handleLeftButtonClick}
+//         // onMouseUp={handleLeftButtonRelease}
+//         onTouchStart={handleLeftButtonClick}
+//         onTouchEnd={handleLeftButtonRelease}
+//         style={{
+//           backgroundImage: `url(${leftButtonImage})`,
+//           backgroundRepeat: "no-repeat",
+//           backgroundSize: "cover",
+//         }}
+//       ></button>
+//       <button
+//         className="rightButtonNav"
+//         // onMouseDown={handleRightButtonClick}
+//         // onMouseUp={handleRightButtonRelease}
+//         onTouchStart={handleRightButtonClick}
+//         onTouchEnd={handleRightButtonRelease}
+//         style={{
+//           backgroundImage: `url(${rightButtonImage})`,
+//           backgroundRepeat: "no-repeat",
+//           backgroundSize: "cover",
+//         }}
+//       ></button>
+
+//       <button
+//         className="upButtonNav"
+//         // onMouseDown={handleUpButtonClick}
+//         // onMouseUp={handleUpButtonRelease}
+//         onTouchStart={handleUpButtonClick}
+//         onTouchEnd={handleUpButtonRelease}
+//         style={{
+//           backgroundImage: `url(${upButtonImage})`,
+//           backgroundRepeat: "no-repeat",
+//           backgroundSize: "cover",
+//         }}
+//       ></button>
+
+//       <button
+//         className="downButtonNav"
+//         // onMouseDown={handleDownButtonClick}
+//         // onMouseUp={handleDownButtonRelease}
+//         onTouchStart={handleDownButtonClick}
+//         onTouchEnd={handleDownButtonRelease}
+//         style={{
+//           backgroundImage: `url(${downButtonImage})`,
+//           backgroundRepeat: "no-repeat",
+//           backgroundSize: "cover",
+//         }}
+//       ></button>
+//     </div>
+//   );
+// };
+
+// export default JoystickButtons;
+
 import React from "react";
 import "./JoystickButtons.css";
 
@@ -25,44 +140,60 @@ const JoystickButtons = () => {
     (state) => state.setIsDownButtonPressed
   );
 
-  const handleLeftButtonClick = () => {
-    setIsLeftButtonPressed(true);
-  };
-  const handleLeftButtonRelease = () => {
-    setIsLeftButtonPressed(false);
-  };
-
-  const handleRightButtonClick = () => {
-    setIsRightButtonPressed(true);
-  };
-  const handleRightButtonRelease = () => {
-    setIsRightButtonPressed(false);
-  };
-
-  const handleUpButtonClick = () => {
-    setIsUpButtonPressed(true);
-  };
-  const handleUpButtonRelease = () => {
-    setIsUpButtonPressed(false);
+  const handleTouchMove = (direction, isPressed) => {
+    switch (direction) {
+      case "left":
+        setIsLeftButtonPressed(isPressed);
+        break;
+      case "right":
+        setIsRightButtonPressed(isPressed);
+        break;
+      case "up":
+        setIsUpButtonPressed(isPressed);
+        break;
+      case "down":
+        setIsDownButtonPressed(isPressed);
+        break;
+      default:
+        break;
+    }
   };
 
-  const handleDownButtonClick = () => {
-    setIsDownButtonPressed(true);
-  };
-  const handleDownButtonRelease = () => {
-    setIsDownButtonPressed(false);
+  const handleTouchStart = (direction) => {
+    handleTouchMove(direction, true);
   };
 
-  /////
+  const handleTouchEnd = (direction) => {
+    handleTouchMove(direction, false);
+  };
+
+  const throttle = (func, delay) => {
+    let lastCalled = 0;
+    return function (...args) {
+      const now = new Date().getTime();
+      if (now - lastCalled < delay) {
+        return;
+      }
+      lastCalled = now;
+      func(...args);
+    };
+  };
+
+  const throttledTouchStart = React.useMemo(
+    () => throttle(handleTouchStart, 200),
+    []
+  );
+  const throttledTouchEnd = React.useMemo(
+    () => throttle(handleTouchEnd, 200),
+    []
+  );
 
   return (
     <div className="joystick-buttons">
       <button
         className="leftButtonNav"
-        // onMouseDown={handleLeftButtonClick}
-        // onMouseUp={handleLeftButtonRelease}
-        onTouchStart={handleLeftButtonClick}
-        onTouchEnd={handleLeftButtonRelease}
+        onTouchStart={() => throttledTouchStart("left")}
+        onTouchEnd={() => throttledTouchEnd("left")}
         style={{
           backgroundImage: `url(${leftButtonImage})`,
           backgroundRepeat: "no-repeat",
@@ -71,10 +202,8 @@ const JoystickButtons = () => {
       ></button>
       <button
         className="rightButtonNav"
-        // onMouseDown={handleRightButtonClick}
-        // onMouseUp={handleRightButtonRelease}
-        onTouchStart={handleRightButtonClick}
-        onTouchEnd={handleRightButtonRelease}
+        onTouchStart={() => throttledTouchStart("right")}
+        onTouchEnd={() => throttledTouchEnd("right")}
         style={{
           backgroundImage: `url(${rightButtonImage})`,
           backgroundRepeat: "no-repeat",
@@ -84,10 +213,8 @@ const JoystickButtons = () => {
 
       <button
         className="upButtonNav"
-        // onMouseDown={handleUpButtonClick}
-        // onMouseUp={handleUpButtonRelease}
-        onTouchStart={handleUpButtonClick}
-        onTouchEnd={handleUpButtonRelease}
+        onTouchStart={() => throttledTouchStart("up")}
+        onTouchEnd={() => throttledTouchEnd("up")}
         style={{
           backgroundImage: `url(${upButtonImage})`,
           backgroundRepeat: "no-repeat",
@@ -97,10 +224,8 @@ const JoystickButtons = () => {
 
       <button
         className="downButtonNav"
-        // onMouseDown={handleDownButtonClick}
-        // onMouseUp={handleDownButtonRelease}
-        onTouchStart={handleDownButtonClick}
-        onTouchEnd={handleDownButtonRelease}
+        onTouchStart={() => throttledTouchStart("down")}
+        onTouchEnd={() => throttledTouchEnd("down")}
         style={{
           backgroundImage: `url(${downButtonImage})`,
           backgroundRepeat: "no-repeat",
