f1930064e60f7aa18ac3d48ee9f15c2ff1f73d4d
alimrts <alimrts07@gmail.com>
2024-05-06 11:37:09 +0300

    image buttons test

--- src/components/FloatingDiv/FloatinDivForIntroRight.jsx
+++ src/components/FloatingDiv/FloatinDivForIntroRight.jsx
@@ -1,11 +1,22 @@
 import React from "react";
-
+import React, { useState } from "react";
 import "./FloatingDiv.css";
 
 const FloatinDivForIntroRight = ({ img, onClick }) => {
+  const [isMobile, setIsMobile] = useState(false);
+  useEffect(() => {
+    const userAgent = navigator.userAgent.toLowerCase();
+    setIsMobile(
+      !!userAgent.match(
+        /android|webos|iphone|ipad|ipod|blackberry|windows phone/i
+      )
+    );
+  }, []);
+
+  const classNames = `floatingDivForIntroRight ${isMobile ? "mobile" : ""}`;
   return (
     // darkMode
-    <div className="floatingDivForIntroRight" onClick={onClick}>
+    <div className={classNames} onClick={onClick}>
       <img src={img} alt="" />
     </div>
   );
