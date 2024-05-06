fcfb88d393d8a813fe3128002b4b51f12e94f1b8
alimrts <alimrts07@gmail.com>
2024-05-06 11:50:59 +0300

    buttons edit

--- src/components/FloatingDiv/FloatinDivForIntroRight.jsx
+++ src/components/FloatingDiv/FloatinDivForIntroRight.jsx
@@ -1,22 +1,11 @@
 import React from "react";
-import React, { useState } from "react";
+
 import "./FloatingDiv.css";
 
 const FloatinDivForIntroRight = ({ img, onClick }) => {
-  const [isMobile, setIsMobile] = useState(false);
-  useEffect(() => {
-    const userAgent = navigator.userAgent.toLowerCase();
-    setIsMobile(
-      !!userAgent.match(
-        /android|webos|iphone|ipad|ipod|blackberry|windows phone/i
-      )
-    );
-  }, []);
-
-  const classNames = `floatingDivForIntroRight ${isMobile ? "mobile" : ""}`;
   return (
     // darkMode
-    <div className={classNames} onClick={onClick}>
+    <div className="floatingDivForIntroRight" onClick={onClick}>
       <img src={img} alt="" />
     </div>
   );
